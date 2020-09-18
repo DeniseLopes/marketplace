fb2ffc6fc53f66d8b0238d12a54b4250ebb942e9
DeniseLopes <denise@kosloski.com.br>
2020-09-18 16:53:22 -0300

    factories

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,13 +12,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        /*DB::table('users')->insert([
                 'name' => 'Administrador',
                 'email' => 'admin@admin.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => 'asasasasasas', 
-        ]);
-       
+        ]);*/
+       factory(\App\User::class, 40)->create();
     }
 }
