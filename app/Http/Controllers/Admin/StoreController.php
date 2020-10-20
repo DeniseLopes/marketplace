c1250fffb406be5ba427f84bfd043e4b7c2c4959
DeniseLopes <denise@kosloski.com.br>
2020-10-19 22:38:52 -0300

    criar loja, routes, controller e blade

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class StoreController extends Controller
 {
     public function index()
     {
-        $stores = \App\Store::all();
-        return $stores;
+        $stores = \App\Store::paginate(10);
+        return view('admin.stores.index',compact('stores'));
+
+    }
+
+    public function create()
+    {
+        $users = \App\User::all(['id', 'name']);
+        
+        return view('admin.stores.create', compact('users'));
+
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $user = \App\User::find($data['user']);
+        $store = $user->store()->create($data);
+
+        return $store;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 //Store Routes
 Route::get('/admin/stores', 'Admin\\StoreController@index');
+Route::get('/admin/stores/create', 'Admin\\StoreController@create');
+Route::post('/admin/stores/store', 'Admin\\StoreController@store');
 //-------------------------------------------------------------
 
 Route::get('/', function () {
