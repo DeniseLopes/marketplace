fd824856ddf097d185841eac066bdfc783f9080f
DeniseLopes <denise@kosloski.com.br>
2021-05-24 19:08:04 -0300

    alteração dos routes para resource store

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,19 @@
 */
 //Store Routes
 Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
+    
+    Route::resource('stores', 'StoreController');
     Route::prefix('stores')->name('stores.')->group(function() {
-
-        Route::get('/', 'StoreController@index')->name('index');
+        Route::put('/restore/{store}', 'StoreController@restore')->name('restore');
+    });
+        /*Route::get('/', 'StoreController@index')->name('index');
         Route::get('create', 'StoreController@create')->name('create');
         Route::post('store', 'StoreController@store')->name('store');
         Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
         Route::post('/update/{store}', 'StoreController@update')->name('update');
         Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
         Route::put('/restore/{store}', 'StoreController@restore')->name('restore');
-    });
+    });*/
 
 //Product Routes
 Route::prefix('products')->name('products.')->group(function() {
